5fa71255dd502bf68756dfbefdc79c8c7e809196
dima <stud0000240024@study.utmn.ru>
2023-09-23 10:40:26 +0500

    remove redundant files

--- LabRab1/LabRab1.py
+++
@@ -1,6 +0,0 @@
-from urllib.request import Request, urlopen
-import json
-
-request_site = Request("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0", headers={"User-Agent": "Mozilla/5.0"})
-webpage = json.loads(urlopen(request_site).read())
-print(webpage['count'])
