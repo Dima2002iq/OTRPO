af82bbe8757c4c842f9f8c5264b1572fd142c233
dima <stud0000240024@study.utmn.ru>
2023-10-07 09:17:38 +0500

    fixed search bug

--- mysite/pokemons/urls.py
+++ mysite/pokemons/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.page, name="index"),
     path("page=<int:num>", views.page),
+    path("search", views.search, name="search"),
     path("<str:name>", views.pokemon, name="pokemon"),
     path("<str:name>/fight", views.fight, name="fight"),
     path("<str:name>/fight/result", views.result, name="result"),
-    path("search", views.search, name="search"),
 ]
